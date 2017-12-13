ec9bd5967669243123e7b7058b337df3cb879a0e
roscolil <ross.lillis@gmail.com>
2017-12-13 17:40:21 +1100

    multiplayer function working

--- js/app.js
+++ js/app.js
@@ -1,38 +1,83 @@
 //wdi_project1_thegame by Ross Lillis
 
 // Message: Player 1 start game by selecting a position on the board
-
-var playerOSelect;
-var playerXSelect;
-var X = 'X';
-var O = 'O';
-
+var player1Select = [];
+var player2Select = [];
+var gameArray = [];
+var player1Wins = 0;
+var player2Wins = 0;
+var numberOfMoves = 0;                            // if numberOfMoves = 9 --- DRAW
 var cellItems = document.querySelectorAll('.grid-container div');
 var newGame = document.querySelector('button');
 
 
-// when plr1 selects
+// var winningCombo =
+// {
+//     winningCombo [1, 2, 3]
+//     winningCombo [4, 5, 6]
+//     winningCombo [7, 8, 9]
+//     winningCombo [1, 4, 7]
+//     winningCombo [2, 5, 8]
+//     winningCombo [3, 6, 9]
+//     winningCombo [1, 5, 9]
+//     winningCombo [3, 5, 7]
+// }
+var changePlayer = function() {
+  numberOfMoves++;
+};
 
-
-var playerO = function(event) {
-  event.target.classList.add('selected-O');
+var playerMove = function(event) {
+  if (numberOfMoves % 2 === 0) {
+    event.target.classList.add('selected-O');
+  }
+  else {
+    event.target.classList.add('selected-X');
+  }
+  changePlayer();
 };
 
 cellItems.forEach(function(cell) {
-  cell.addEventListener('click', playerO );
-});
-
-
-var playerX = function(event) {
-  event.target.classList.add('selected-X');
-};
-
-cellItems.forEach(function(cell) {
-  cell.addEventListener('click', playerX);
+  cell.addEventListener('click', playerMove);
 });
 
 
 
+// var player2Move = function(event) {
+//   event.target.classList.add('selected-X');
+//   changePlayer()
+// };
+
+// cellItems.forEach(function(cell) {
+//   cell.addEventListener('click', player2Move);
+// });
+
+
+// var multiPlayerFunction = function() {
+//   if (numberOfMoves % 2 === 1) {
+//     changePlayer();
+//   }
+//   else if (numberOfMoves % 2 === 0) {
+//     changePlayer();
+//   }
+// };
+
+//multiPlayerFunction();
+
+
+
+
+
+
+// var changePlayer = function() {
+//   if (symbol === 'X') {
+//     // increment playerO by 1
+//     // change symbol to O
+//   } else {
+//     // increment playerX by 1
+//     // change symbol to X
+//   }
+// };
+//
 
 
 
