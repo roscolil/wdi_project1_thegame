c8e8cf76e236d296f560dc75b009863eb78d3104
roscolil <ross.lillis@gmail.com>
2017-12-13 15:23:08 +1100

    more styling and began game logic

--- js/app.js
+++ js/app.js
@@ -2,57 +2,49 @@
 
 // Message: Player 1 start game by selecting a position on the board
 
-// Declare DOM element variables
-var cell1 = document.querySelector('.cell1');   // Can i use querySelectorAll here??
-var cell2 = document.querySelector('.cell2');
-var cell3 = document.querySelector('.cell3');
-var cell4 = document.querySelector('.cell4');
-var cell5 = document.querySelector('.cell5');
-var cell6 = document.querySelector('.cell6');
-var cell7 = document.querySelector('.cell7');
-var cell8 = document.querySelector('.cell8');
-var cell9 = document.querySelector('.cell9');
+var playerOSelect;
+var playerXSelect;
+var X = 'X';
+var O = 'O';
+
+var cellItems = document.querySelectorAll('.grid-container div');
 var newGame = document.querySelector('button');
 
-var playArray = [];
+
+// when plr1 selects
 
 
-// clickHandler function plr1/plr2?
-// winCounter function for plr1/plr2
-// newGame function
+var playerO = function(event) {
+  event.target.classList.add('selected-O');
+};
+
+cellItems.forEach(function(cell) {
+  cell.addEventListener('click', playerO );
+});
 
 
+var playerX = function(event) {
+  event.target.classList.add('selected-X');
+};
 
-
-
-function clickHandler(event) {
-  event.target. = '';                     // Need target and action
-}
-listItems.forEach(function(elem) {
-  elem.addEventListener('click', ClickHandler);
+cellItems.forEach(function(cell) {
+  cell.addEventListener('click', playerX);
 });
 
 
 
-var winCount = function() {               // Needs to account for two players
+
+
+
+
+var winCount = function() {               // Needs to account for two players. Two separate functions??
   var wins = 0;
   wins += 1;
   document.getElementById('wins').innerHTML = wins;
 };
 
+
+
 var newGame = function () {
   var newGame = document.querySelector('');
 };
-
-
-// Event Listeners
-cell1.addEventListener('click', clickHandler);
-cell2.addEventListener('click', clickHandler);
-cell3.addEventListener('click', clickHandler);
-cell4.addEventListener('click', clickHandler);
-cell5.addEventListener('click', clickHandler);
-cell6.addEventListener('click', clickHandler);
-cell7.addEventListener('click', clickHandler);
-cell8.addEventListener('click', clickHandler);
-cell9.addEventListener('click', clickHandler);
-resetButton.addEventListener('click', newGame);
